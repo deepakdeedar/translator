224ae1480ed7cac940c7497c70000aed3df7f755
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-09-03 11:29:46 +0530

    filter intro

---
+++ filter.py
@@ -0,0 +1,14 @@
+my_list = [1, 2, 3]
+
+
+def multiply_by2(item):
+    return item*2
+
+
+def only_odd(item):
+    return item % 2 != 0
+
+
+print(list(map(multiply_by2, my_list)))
+print(list(filter(only_odd, my_list)))
+print(my_list)
